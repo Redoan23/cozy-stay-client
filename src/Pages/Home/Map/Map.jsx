d8ab383fac92788aa489759de48f64f686b36ee3
Redoan23 <redoan1080@gmail.com>
2024-05-10 14:41:47 +0600

    added featured rooms

--- src/Pages/Home/Map/Map.jsx
+++ src/Pages/Home/Map/Map.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
 
