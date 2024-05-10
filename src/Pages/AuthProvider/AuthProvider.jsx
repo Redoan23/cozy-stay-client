59d83f920aab47e4fd7c6fbe1a4ed371bc297d96
Redoan23 <redoan1080@gmail.com>
2024-05-10 20:03:19 +0600

    added api keys

--- src/Pages/AuthProvider/AuthProvider.jsx
+++ src/Pages/AuthProvider/AuthProvider.jsx
@@ -1,13 +1,40 @@
-import React, { createContext } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import auth from '../../firebase/firebase';
+
 
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({ children }) => {
 
-    
+    const googleAuthProvider = new GoogleAuthProvider()
+    const [user, setUser] = useState(null)
 
-    const AuthInfo = []
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const loginUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const logOutUser = () => {
+        return signOut(auth)
+    }
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleAuthProvider)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const AuthInfo = [createUser, loginUser, logOutUser, googleSignIn]
 
     return (
         <AuthContext.Provider value={AuthInfo} >
