b6823d6c6592cd1c398b8368795d2278a43f2bb8
Redoan23 <redoan1080@gmail.com>
2024-05-12 06:48:56 +0600

    added my bookings cover image

--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const MyBookings = () => {
     return (
         <div>
-            this is my booking page
+            <div className=' bg-[url("https://i.ibb.co/DQkWbGT/hotel-room-unsplash.jpg")] h-[600px] w-full bg-cover'>
+                <div className=' text-white bg-gradient-to-b from-[#151515ac] to-[#2a29293e] h-full w-full flex flex-col items-center justify-center'>
+                    <h3 className=' font-semibold text-5xl text-center pb-5'>All Your Bookings At one Place</h3>
+                    <p className=' text-sm text-center text-yellow-500'>manage your bookings like never before</p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
